21ef9c67a092d0a298e7507cb1084b194c471069
nhinguyen04 <valentinetwin94@gmail.com>
2023-02-21 17:43:30 -0800

    completed 1-3

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,29 @@
-// Your code here
+const phaseModulePath = "./dragon.js";
+const Dragon = require(phaseModulePath);
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        for(const index in this.evilDoings) {
+            console.log(`${this.name} will ${this.evilDoings[index]}`);
+        }
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+}
+
+
+/****************************************************************************/
+
+try {
+    module.exports = EvilDragon;
+  } catch {
+    module.exports = null;
+  }
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,29 @@
-// Your code here
+const phaseModulePath = "./dragon.js";
+const Dragon = require(phaseModulePath);
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach((ele) => {
+            console.log(`${this.name} likes to ${ele}`);
+        })
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+}
+
+
+/****************************************************************************/
+
+try {
+    module.exports = FriendlyDragon;
+  } catch {
+    module.exports = null;
+  }
